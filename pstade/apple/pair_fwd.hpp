1856ff6fefac620006c823e955aad2c7bd8b8c96
Fernando Pelliccioni <fpelliccioni@gmail.com>
2013-09-26 12:09:46 -0300

    Avoid the std::pair forward declaration if LibC++ is being used

--- pstade/apple/pair_fwd.hpp
+++ pstade/apple/pair_fwd.hpp
@@ -1,14 +1,15 @@
 #ifndef PSTADE_APPLE_PAIR_FWD_HPP
 #define PSTADE_APPLE_PAIR_FWD_HPP
 
-
 // PStade.Apple
 //
 // Copyright Shunsuke Sogame 2005-2006.
+// Copyright (C) 2013 Fernando Pelliccioni (fpelliccioni@gmail.com) 
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
+#ifndef _LIBCPP_VERSION
 
 namespace std {
 
@@ -19,5 +20,6 @@ struct pair;
 
 } // namespace std
 
+#endif // _LIBCPP_VERSION
 
-#endif
+#endif // PSTADE_APPLE_PAIR_FWD_HPP
